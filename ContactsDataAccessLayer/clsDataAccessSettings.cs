bb4816b93083d735531b4c14c47be0ac2d93f694
Moath <85872949+pep15@users.noreply.github.com>
2025-12-17 16:22:30 -0500

    Release_Desktop_App

--- ContactsDataAccessLayer/clsDataAccessSettings.cs
+++ ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -5,6 +5,6 @@ namespace ContactsDataAccessLayer
     internal class clsDataAccessSettings
     {
 
-        public static string ConnectionString = "Server=sqlserver;Database=DVLD;User Id=sa;Password=MyStrongP@ssw0rd!;TrustServerCertificate=True";
+        public static string ConnectionString = "Server=172.17.0.1,1433;Database=DVLD;User Id=sa;Password=MyStrongP@ssw0rd!;TrustServerCertificate=True";
     }
 }
